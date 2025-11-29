f95aec45c8e70348b696cf0ed86f8b4a88dcb694
gefeeHome <megamisha41521@gmail.com>
2025-11-29 23:18:08 +0300

    add path tg-auth.html

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -5,14 +5,14 @@ import { useUserStore } from '../data/user.js'
 import { useTelegram } from '../composables/useTelegram.js'
 
 const userStore = useUserStore()
-const { userData, initDataRaw } = useTelegram() // убрать initDataRaw при разработке
+const { userData, /*initDataRaw*/ } = useTelegram() // убрать initDataRaw при разработке
 
 /* Только для разработки*/
-/*const fakeInitData = "user=%7B%22id%22%3A909844183%2C%22first_name%22%3A%22Miska%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22gefeeRu%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F2cXXMBKksn2jvadNJFGJLUMjCLWWa6wcUhCZPT962M8.svg%22%7D&chat_instance=-6139284978316757086&chat_type=sender&auth_date=1755960241&signature=aA673g3WFjZ7Mu1-8HhKTZfIaPye5lxYnf44Dgmjtmnbv9YEP3ihlhxTivVoZp1Pm0wJVZhMlJYqdNy2A1_CAw&hash=63bbaca6ddc2c8c6be6a4f8c130ec401b0838b8723390a5aaec07c375dea2af3";
+const fakeInitData = "user=%7B%22id%22%3A909844183%2C%22first_name%22%3A%22Miska%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22gefeeRu%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F2cXXMBKksn2jvadNJFGJLUMjCLWWa6wcUhCZPT962M8.svg%22%7D&chat_instance=-6139284978316757086&chat_type=sender&auth_date=1755960241&signature=aA673g3WFjZ7Mu1-8HhKTZfIaPye5lxYnf44Dgmjtmnbv9YEP3ihlhxTivVoZp1Pm0wJVZhMlJYqdNy2A1_CAw&hash=63bbaca6ddc2c8c6be6a4f8c130ec401b0838b8723390a5aaec07c375dea2af3";
 const initDataRaw = fakeInitData
-*/
 
-const initDataSend = String(initDataRaw.value) /* .value если не fake*/
+
+const initDataSend = String(initDataRaw) /* .value если не fake*/
 
 const setAuthenticated = inject('setAuthenticated')
 
