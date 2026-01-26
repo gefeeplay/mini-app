41f00491392a5b6ed08dd7e5808aa885c99dc556
gefeeHome <megamisha41521@gmail.com>
2026-01-26 20:12:11 +0300

    add api config file

---
+++ src/config/api.config.js
@@ -0,0 +1,10 @@
+// API URLs Configuration
+export const API_CONFIG = {
+  AUTH_API_URL: 'https://gainly.site/auth/api/auth',
+  AUTH_REFRESH_URL: 'https://gainly.site/auth/api/auth/refresh',
+  AVATARS_API_URL: 'https://gainly-avatars.fly.dev/api',
+  WORKOUT_API_URL: 'https://fitness-app-workout-api.fly.dev/api/Workout',
+  USER_API_URL: 'https://gainly.site/auth/api/user',
+  FRIENDS_API_URL: 'https://gainly.site/auth/api/friends'
+}
+
